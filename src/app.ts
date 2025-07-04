17b2cbda1159578ccb6b22c1ae782b4814d44ab7
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-07-04 21:04:10 +0600

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://bookcrate-library-management.netlify.app'],
     credentials: true
 }));
 
